b967e53d483343dd39cd74b99e70b09607819521
Szymon Mueller <szymon.mueller@man.poznan.pl>
2017-08-17 10:15:28 +0200

    Changing popularity repo definition and adding fake popularity object

---
+++ src/main/java/eu/h2020/symbiote/ranking/PopularityObject.java
@@ -0,0 +1,17 @@
+package eu.h2020.symbiote.ranking;
+
+/**
+ * Created by Szymon Mueller on 17/08/2017.
+ */
+public class PopularityObject {
+
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
--- src/main/java/eu/h2020/symbiote/ranking/PopularityRepository.java
+++ src/main/java/eu/h2020/symbiote/ranking/PopularityRepository.java
@@ -1,6 +1,5 @@
 package eu.h2020.symbiote.ranking;
 
-import eu.h2020.symbiote.cloud.monitoring.model.CloudMonitoringDevice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +7,6 @@ import org.springframework.stereotype.Repository;
  * Created by Szymon Mueller on 01/08/2017.
  */
 @Repository
-public interface PopularityRepository extends MongoRepository<String,String> {
+public interface PopularityRepository extends MongoRepository<PopularityObject, String> {
 
 }
